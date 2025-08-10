6d0132d8e1217c779cd5966cbae95ac877b5928b
farsmellah <the.real.smartfellah@gmail.com>
2025-08-10 20:04:27 +0300

    feat: add api entities config

---
+++ config/wb.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'resources' => [
+        'orders' => [
+            'table' => 'orders',
+            'limit' => 500,
+        ],
+        'sales' => [
+            'table' => 'sales',
+            'limit' => 500,
+        ],
+        'stocks' => [
+            'table' => 'stocks',
+            'limit' => 500,
+        ],
+        'incomes' => [
+            'table' => 'incomes',
+            'limit' => 500,
+        ],
+    ],
+];
