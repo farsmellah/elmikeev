2bbfcec79c50f8304b80f69f62719eeacf79cdfc
farsmellah <the.real.smartfellah@gmail.com>
2025-08-10 20:03:54 +0300

    feat: add payload hash to avoid duplicated insertion

--- database/migrations/2025_08_10_094131_create_incomes_table.php
+++ database/migrations/2025_08_10_094131_create_incomes_table.php
@@ -10,6 +10,7 @@ return new class extends Migration {
         Schema::create('incomes', function (Blueprint $table)
         {
             $table->id();
+            $table->string('payload_hash')->unique();
             $table->json('payload');
             $table->timestamps();
         });
--- database/migrations/2025_08_10_094153_create_orders_table.php
+++ database/migrations/2025_08_10_094153_create_orders_table.php
@@ -10,6 +10,7 @@ return new class extends Migration {
         Schema::create('orders', function (Blueprint $table)
         {
             $table->id();
+            $table->string('payload_hash')->unique();
             $table->json('payload');
             $table->timestamps();
         });
